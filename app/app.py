8a5fa8ad1a5d9f3f9a46c047f7f80eadefe49e29
Ahmad Alfakharany <abohmeed@gmail.com>
2018-08-12 23:07:39 +0200

    Installs json2csv package

--- app/app.py
+++ app/app.py
@@ -3,26 +3,16 @@ from bs4 import BeautifulSoup
 from flask import Flask
 from flask import request
 from flask import jsonify
+import urllib
 
 app = Flask(__name__)
-# TODO: add API functionality to the file
 # TODO: convert the search query to be URL-friendly
-
-query = "beautifulsoup"
-r = requests.get("https://www.google.com/search?q=" + query)
-bs = BeautifulSoup(r.text, features="html.parser")
-
-for link in bs.find_all('div', {'class': 'g'}):
-    title = link.find('a')
-    url = link.find('cite')
-    if title and url:
-        print title.text, url.text
 
 
 @app.route('/', methods=['POST'])
 def index():
     response = []
-    query = request.form['query']
+    query = urllib.quote_plus(request.form['query'])
     r = requests.get("https://www.google.com/search?q=" + query)
     bs = BeautifulSoup(r.text, features="html.parser")
     for link in bs.find_all('div', {'class': 'g'}):
