7fe911621ed7601cb8328c79632a4bd98d8e12cf
Ahmad Alfakharany <abohmeed@gmail.com>
2018-08-12 22:32:07 +0200

    The Flask application is accepting post requests and responding back in JSON

--- app/app.py
+++ app/app.py
@@ -1,14 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-#TODO: add API functionality to the file
-#TODO: convert the search query to be URL-friendly
+from flask import Flask
+from flask import request
+from flask import jsonify
 
+app = Flask(__name__)
+# TODO: add API functionality to the file
+# TODO: convert the search query to be URL-friendly
 
-r = requests.get("https://www.google.com/search?q=beautifulsoup")
-bs = BeautifulSoup(r.text,features="html.parser")
+query = "beautifulsoup"
+r = requests.get("https://www.google.com/search?q=" + query)
+bs = BeautifulSoup(r.text, features="html.parser")
 
-for link in bs.find_all('div',{'class':'g'}):
+for link in bs.find_all('div', {'class': 'g'}):
     title = link.find('a')
     url = link.find('cite')
     if title and url:
         print title.text, url.text
+
+
+@app.route('/', methods=['POST'])
+def index():
+    response = []
+    query = request.form['query']
+    r = requests.get("https://www.google.com/search?q=" + query)
+    bs = BeautifulSoup(r.text, features="html.parser")
+    for link in bs.find_all('div', {'class': 'g'}):
+        title = link.find('a')
+        url = link.find('cite')
+        if title and url:
+            response.append({'title':title.text,'url':url.text})
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
