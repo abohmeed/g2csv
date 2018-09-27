68c2bab8336ca8da763ecf4aad015013dab99fbc
Ahmed El-Fakharany <ahmed.elfakharany@zlien.com>
2018-09-27 16:59:54 +0200

    Divides the application into two separate Docker images

---
+++ middleware/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const fs = require('fs');
+const Json2csvParser = require('json2csv').Parser;
+const fields = [
+    {
+        label: 'Title',
+        value: 'title'
+    },
+    {
+        label: 'URL',
+        value: 'url'
+    }
+];
+const request = require('request');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.render("index", { query: null })
+})
+
+app.post('/', function (req, res) {
+    let squery = req.body.search
+    request.post({ url: "http://scraper:5000",json:true, form: { query: squery } }, function (err, pres, body) {
+        if (err) {
+            res.render('index', { query: squery });
+            console.log(err)
+        } else {
+            const parser = new Json2csvParser({ fields });
+            const csv = parser.parse(body);
+            var headers = {
+                'Content-type': 'application/octet-stream',
+                'Content-Disposition': 'attachment; filename=result.csv'
+            };
+            res.writeHead(200, headers);
+            res.end(csv);
+        }
+    })
+
+})
+
+app.listen(3000, function () {
+    console.log('G2CSV listening on port 3000')
+})
---
+++ scraper/app.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask
+from flask import request
+from flask import jsonify
+import urllib
+
+app = Flask(__name__)
+# TODO: convert the search query to be URL-friendly
+
+
+@app.route('/', methods=['POST'])
+def index():
+    response = []
+    query = urllib.quote_plus(request.form['query'])
+    r = requests.get("https://www.google.com/search?q=" + query)
+    bs = BeautifulSoup(r.text, features="html.parser")
+    for link in bs.find_all('div', {'class': 'g'}):
+        title = link.find('a')
+        url = link.find('cite')
+        if title and url:
+            response.append({'title':title.text,'url':url.text})
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
