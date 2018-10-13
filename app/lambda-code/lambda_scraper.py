f4d026892890171a74bd77affe3685916d79ed0a
Ahmad El Fakharany <abohmeed@gmai.com>
2018-10-13 14:04:26 +0200

    The function now is tested on Lambda successfully. We just need to set the timeout for more than 3 seconds

--- app/lambda-code/lambda_scraper.py
+++ app/lambda-code/lambda_scraper.py
@@ -9,8 +9,10 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def scrape(keywords):
+    logger.info(keywords)
     response = []
     for k in keywords:
+        logger.info("Parsing "+k)
         r = requests.get("https://www.google.com/search?q=" + k)
         logging.info("Successfully contacted Google.com")
         bs = BeautifulSoup(r.text, features="html.parser")
@@ -23,7 +25,7 @@ def scrape(keywords):
     return json.dumps(response,sort_keys=True, indent=4)
 
 def main(event,context):    
-    # logger.info(event)
+    logger.info(event)
     # return scrape(event['keywords'])    
     key = event["Records"][0]["s3"]["object"]["key"]
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
@@ -31,7 +33,8 @@ def main(event,context):
     s3.Object(bucket,key).download_file('/tmp/keywords.txt')
     with open('/tmp/keywords.txt') as f:
         lines = [line.rstrip('\n') for line in f]
-    print scrape(lines)
+    result = scrape(lines)
+    logger.info(result)
 
-# event = {u'Records': [{u'eventVersion': u'2.0', u'eventTime': u'2018-10-10T14:31:14.736Z', u'requestParameters': {u'sourceIPAddress': u'41.33.59.145'}, u's3': {u'configurationId': u'035518bf-df9b-4fb7-bafa-227bef9d7793', u'object': {u'eTag': u'c680e919b86b379457a309f10da72376', u'sequencer': u'005BBE0D32A3A8E820', u'key': u'.bash_history', u'size': 183}, u'bucket': {u'arn': u'arn:aws:s3:::gscraper.uploads', u'name': u'gscraper.uploads', u'ownerIdentity': {u'principalId': u'A2AHDBAACARLZN'}}, u's3SchemaVersion': u'1.0'}, u'responseElements': {u'x-amz-id-2': u'PvX+43FiSZFMoaanCaS/lqP+ue9Y/3q/T3BwrMgHQPSHt4n4JLoAdbxA97xt1+JN6Lbo4zxqybw=', u'x-amz-request-id': u'341A81C199531AF7'}, u'awsRegion': u'us-west-2', u'eventName': u'ObjectCreated:Put', u'userIdentity': {u'principalId': u'A2AHDBAACARLZN'}, u'eventSource': u'aws:s3'}]}
+# event = {u'Records': [{u'eventVersion': u'2.0', u'eventTime': u'2018-10-13T11:49:42.272Z', u'requestParameters': {u'sourceIPAddress': u'156.197.36.206'}, u's3': {u'configurationId': u'035518bf-df9b-4fb7-bafa-227bef9d7793', u'object': {u'eTag': u'04e44f2d78b56ff4de6a09fa7694f0e3', u'sequencer': u'005BC1DBD63BC17945', u'key': u'Keywords.txt', u'size': 19}, u'bucket': {u'arn': u'arn:aws:s3:::gscraper.uploads', u'name': u'gscraper.uploads', u'ownerIdentity': {u'principalId': u'A2AHDBAACARLZN'}}, u's3SchemaVersion': u'1.0'}, u'responseElements': {u'x-amz-id-2': u'uQhECxvKRsFdytmpCWG1W49MIPcx3EmwIUkAzCmqXWqaegUzJExpt8xWTwmISZW78qoLvVS2gfI=', u'x-amz-request-id': u'806D4BCED136AC00'}, u'awsRegion': u'us-west-2', u'eventName': u'ObjectCreated:Put', u'userIdentity': {u'principalId': u'A2AHDBAACARLZN'}, u'eventSource': u'aws:s3'}]}
 # main(event,"")
