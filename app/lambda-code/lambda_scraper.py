2be8af362f0a1c5c19a76a16f430863fefd20f96
Ahmed El-Fakharany <ahmed.elfakharany@zlien.com>
2018-10-18 16:45:58 +0200

    Lambda now outputs the result in the form of CSV file

--- app/lambda-code/lambda_scraper.py
+++ app/lambda-code/lambda_scraper.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import logging
 import boto3
+import csv
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -21,7 +22,8 @@ def scrape(keywords):
             if title and url:
                 logging.info("Parsed %s", title.text)
                 response.append({'keyword':k,'title':title.text,'url':url.text})
-    return json.dumps(response,sort_keys=True, indent=4)
+    return response
+    
 
 def main(event,context):    
     logger.info(event)
@@ -32,11 +34,13 @@ def main(event,context):
     with open('/tmp/keywords.txt') as f:
         lines = [line.rstrip('\n') for line in f]
     result = scrape(lines)
-    with open('/tmp/keywords.json', 'w') as f:
-        f.write(result)
+    with open('/tmp/keywords.csv', 'wb') as f:
+        w = csv.DictWriter(f, result[0].keys())
+        w.writeheader()
+        w.writerows(result)
     s3 = boto3.client('s3')
-    s3.upload_file('/tmp/keywords.json', 'gscraper.downloads', key + ".json")
+    s3.upload_file('/tmp/keywords.csv', 'gscraper.downloads', key + ".csv")
 
 
-# event = {u'Records': [{u'eventVersion': u'2.0', u'eventTime': u'2018-10-13T11:49:42.272Z', u'requestParameters': {u'sourceIPAddress': u'156.197.36.206'}, u's3': {u'configurationId': u'035518bf-df9b-4fb7-bafa-227bef9d7793', u'object': {u'eTag': u'04e44f2d78b56ff4de6a09fa7694f0e3', u'sequencer': u'005BC1DBD63BC17945', u'key': u'Keywords.txt', u'size': 19}, u'bucket': {u'arn': u'arn:aws:s3:::gscraper.uploads', u'name': u'gscraper.uploads', u'ownerIdentity': {u'principalId': u'A2AHDBAACARLZN'}}, u's3SchemaVersion': u'1.0'}, u'responseElements': {u'x-amz-id-2': u'uQhECxvKRsFdytmpCWG1W49MIPcx3EmwIUkAzCmqXWqaegUzJExpt8xWTwmISZW78qoLvVS2gfI=', u'x-amz-request-id': u'806D4BCED136AC00'}, u'awsRegion': u'us-west-2', u'eventName': u'ObjectCreated:Put', u'userIdentity': {u'principalId': u'A2AHDBAACARLZN'}, u'eventSource': u'aws:s3'}]}
-# main(event,"")
+event = {u'Records': [{u'eventVersion': u'2.0', u'eventTime': u'2018-10-18T14:07:00.676Z', u'requestParameters': {u'sourceIPAddress': u'41.33.59.145'}, u's3': {u'configurationId': u'895e0457-7b21-48af-a6b7-70bda7d48e7c', u'object': {u'eTag': u'512278359caf9bcdeee50a98d813ed42', u'sequencer': u'005BC89384A152327E', u'key': u'keywords.txt', u'size': 26}, u'bucket': {u'arn': u'arn:aws:s3:::gscraper.uploads', u'name': u'gscraper.uploads', u'ownerIdentity': {u'principalId': u'A2AHDBAACARLZN'}}, u's3SchemaVersion': u'1.0'}, u'responseElements': {u'x-amz-id-2': u'E5HHXp/5RdzELVyxKKzeoTaozIdo0nrpoGP4BYsxzBBBN8GFH0Ya1HoFrGrZOMSSMzBiTVRcYY8=', u'x-amz-request-id': u'F93C166AFB381653'}, u'awsRegion': u'us-west-2', u'eventName': u'ObjectCreated:Put', u'userIdentity': {u'principalId': u'A2AHDBAACARLZN'}, u'eventSource': u'aws:s3'}]}
+main(event,"")
