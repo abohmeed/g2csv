65de7ceeea8f089fb4ae9e77824fe3c9434030a6
Ahmed El-Fakharany <ahmed.elfakharany@zlien.com>
2018-10-23 15:22:05 +0200

    Creates new container for Nginx server and truncates app.py to recieve requests directly from Angular

--- app/app.py
+++ app/app.py
@@ -4,23 +4,17 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import urllib
+import boto3
+import hashlib
 
 app = Flask(__name__)
-# TODO: convert the search query to be URL-friendly
-
 
 @app.route('/', methods=['POST'])
 def index():
     response = []
     query = urllib.quote_plus(request.form['query'])
-    r = requests.get("https://www.google.com/search?q=" + query)
-    bs = BeautifulSoup(r.text, features="html.parser")
-    for link in bs.find_all('div', {'class': 'g'}):
-        title = link.find('a')
-        url = link.find('cite')
-        if title and url:
-            response.append({'title':title.text,'url':url.text})
-    return jsonify(response)
+    key = hashlib.sha224(query).hexdigest()
+    return query    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/lambda-code/lambda_scraper.py
+++ app/lambda-code/lambda_scraper.py
@@ -27,19 +27,14 @@ def scrape(keywords):
 
 def main(event,context):    
     logger.info(event)
-    key = event["Records"][0]["s3"]["object"]["key"]
-    bucket = event["Records"][0]["s3"]["bucket"]["name"]
-    s3 = boto3.resource('s3')
-    s3.Object(bucket,key).download_file('/tmp/keywords.txt')
-    with open('/tmp/keywords.txt') as f:
-        lines = [line.rstrip('\n') for line in f]
-    result = scrape(lines)
+    keywords = event["Records"][0]["sns"]["message"]["keywords"]
+    result = scrape(keywords)
     with open('/tmp/keywords.csv', 'wb') as f:
         w = csv.DictWriter(f, result[0].keys())
         w.writeheader()
         w.writerows(result)
     s3 = boto3.client('s3')
-    s3.upload_file('/tmp/keywords.csv', 'gscraper.downloads', key + ".csv")
+    s3.upload_file('/tmp/keywords.csv', 'gscraper.downloads/' + + '.csv")
 
 
 # event = {u'Records': [{u'eventVersion': u'2.0', u'eventTime': u'2018-10-18T14:07:00.676Z', u'requestParameters': {u'sourceIPAddress': u'41.33.59.145'}, u's3': {u'configurationId': u'895e0457-7b21-48af-a6b7-70bda7d48e7c', u'object': {u'eTag': u'512278359caf9bcdeee50a98d813ed42', u'sequencer': u'005BC89384A152327E', u'key': u'keywords.txt', u'size': 26}, u'bucket': {u'arn': u'arn:aws:s3:::gscraper.uploads', u'name': u'gscraper.uploads', u'ownerIdentity': {u'principalId': u'A2AHDBAACARLZN'}}, u's3SchemaVersion': u'1.0'}, u'responseElements': {u'x-amz-id-2': u'E5HHXp/5RdzELVyxKKzeoTaozIdo0nrpoGP4BYsxzBBBN8GFH0Ya1HoFrGrZOMSSMzBiTVRcYY8=', u'x-amz-request-id': u'F93C166AFB381653'}, u'awsRegion': u'us-west-2', u'eventName': u'ObjectCreated:Put', u'userIdentity': {u'principalId': u'A2AHDBAACARLZN'}, u'eventSource': u'aws:s3'}]}
