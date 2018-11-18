c40342c14df9ec6278b38a7ed53ea7ca520b9319
Ahmad El Fakharany <abohmeed@gmai.com>
2018-11-18 23:29:24 +0200

    Changes docker-compose to build the images

--- scraper/app.py
+++ scraper/app.py
@@ -4,15 +4,20 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import urllib
+import logging
 
 app = Flask(__name__)
 # TODO: convert the search query to be URL-friendly
 
+logger = logging.getLogger()
+logger.setLevel('INFO')
+logging.basicConfig()
 
 @app.route('/', methods=['POST'])
 def index():
     response = []
-    query = urllib.quote_plus(request.form['query'])
+    query = request.form['query']
+    logger.info(query)
     r = requests.get("https://www.google.com/search?q=" + query)
     bs = BeautifulSoup(r.text, features="html.parser")
     for link in bs.find_all('div', {'class': 'g'}):
@@ -21,6 +26,7 @@ def index():
         if title and url:
             response.append({'title':title.text,'url':url.text})
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
